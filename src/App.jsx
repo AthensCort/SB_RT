c535afebc89c19b0ba00a463181533738ff509b4
Athens Cort <athens.cort@gmail.com>
2026-04-23 10:09:19 -0600

    FINAL PART D

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { supabase } from './lib/supabaseClient'
 import Scoreboard   from './components/Scoreboard'
 import EventFeed    from './components/EventFeed'
 import NewEventForm from './components/NewEventForm'
+import StatsPanel from './components/StatsPanel'
 
 export default function App() {
   const [match,  setMatch]  = useState(null)
@@ -124,6 +125,7 @@ export default function App() {
         onReset={resetScore}
       />
 
+      <StatsPanel events={events} />    {/* [D] */}
       <NewEventForm />
 
       <EventFeed events={events} />
